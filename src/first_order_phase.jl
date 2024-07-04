24f6aa46e299648c94716ffd1e78eb416c2e5e11
Eduardo <grossi.eduard@gmail.com>
2024-07-04 19:24:35 +0200

    added linear term

--- src/first_order_phase.jl
+++ src/first_order_phase.jl
@@ -1,18 +1,21 @@
 include("discretization.jl")
 include("functions.jl")
+using QuadGK
+using Integrals
 
-N= 10
-Rmax=2
+N= 100
+Rmax=4
 d=3
 Λ=10
 mass=-10
 lam=4 
 H=1
 L=InfiniteVolume()
-ϕ =SymmetricInterval(N,Rmax).x
+grid=SymmetricInterval(N,Rmax)
+ϕ =grid.x
 U0 = @. H+ mass*ϕ + lam*ϕ^3
-p=(SymmetricInterval(N,Rmax),FlowEquation(d, Λ, L))
-tspan = (0,40)
+p=(grid,FlowEquation(d, Λ, L))
+tspan = (0,200)
 prob = ODEProblem(not_symmetric,U0,tspan,p)
 
 sol = solve(prob)
@@ -20,3 +23,22 @@ sol = solve(prob)
 using Plots
 
 plot(sol.u)
+
+function integrale(sol,disc,idx,t)
+    
+    y=sol(t)[1:idx]
+    x= disc.x[1:idx]
+    
+    solve(SampledIntegralProblem(y, x), TrapezoidalRule()).u
+end
+
+integrale(sol,grid,2,1)
+
+integrale.(Ref(sol),Ref(grid),2:100,Ref(1))
+
+p1=plot(integrale.(Ref(sol),Ref(grid),2:100,Ref(1)))
+for t  in 1:3:100
+ plot!(p1,integrale.(Ref(sol),Ref(grid),2:100,Ref(t)))
+ xlims!(25,75)
+end     
+p1
