c56798a4747eacd06168880040e2b4fd607caa0f
Eduardo <grossi.eduard@gmail.com>
2024-07-07 21:53:10 +0200

    crazy stuff

--- src/first_order_phase.jl
+++ src/first_order_phase.jl
@@ -1,32 +1,49 @@
 include("discretization.jl")
-include("functions.jl")
+include("functions_new.jl")
 using QuadGK
 using Integrals
 
-N= 100
-Rmax=4
+N= 400
+Rmax=2
 d=3
 Λ=10
-mass=-10
+mass=-1
 lam=4 
-H=1
+H=-10
 L=InfiniteVolume()
 grid=SymmetricInterval(N,Rmax)
 ϕ =grid.x
 U0 = @. H+ mass*ϕ + lam*ϕ^3
+plot(ϕ,U0)
+u0=transpose(stack(((U0,zeros(length(U0))))))
+
 p=(grid,FlowEquation(d, Λ, L))
-tspan = (0,200)
-prob = ODEProblem(not_symmetric,U0,tspan,p)
+tspan = (0,400)
+prob = ODEProblem(not_symmetric,u0,tspan,p)
 
 sol = solve(prob)
 
 using Plots
+plot(sol.u[1][1,:])
+plot(sol.u[end][1,:])
 
-plot(sol.u)
+p1=plot(sol.u[2][2,:])
+for t  in 3:2:length(sol.t)
+   plot!(p1,sol.u[t][2,:])
+end
+p1
 
-function integrale(sol,disc,idx,t)
+p1=plot(sol.u[2][1,:])
+for t  in 3:20:length(sol.t)
+   plot!(p1,sol.u[t][1,:])
+end
+p1
+savefig(p1,"fancy_Stuff.pdf")
+plot(sol.u[end][1,:])
+
+function integrale_potential(sol,disc,idx,t)
     
-    y=sol(t)[1:idx]
+    y=sol(t)[1,1:idx]
     x= disc.x[1:idx]
     
     solve(SampledIntegralProblem(y, x), TrapezoidalRule()).u
@@ -36,9 +53,9 @@ integrale(sol,grid,2,1)
 
 integrale.(Ref(sol),Ref(grid),2:100,Ref(1))
 
-p1=plot(integrale.(Ref(sol),Ref(grid),2:100,Ref(1)))
+p1=plot(integrale.(Ref(sol),Ref(grid),2:400,Ref(1)))
 for t  in 1:3:100
- plot!(p1,integrale.(Ref(sol),Ref(grid),2:100,Ref(t)))
- xlims!(25,75)
+ plot!(p1,integrale.(Ref(sol),Ref(grid),2:400,Ref(t)))
+ #xlims!(25,75)
 end     
 p1
